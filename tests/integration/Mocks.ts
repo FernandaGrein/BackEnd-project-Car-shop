9b116d6b8f55891bd45abf3ff3f05405d37c3758
Fernanda Grein <fernandagrein.fgm@outlook.com>
2022-11-21 14:56:53 -0300

    adiciona testes de integração rota get all cars

--- tests/integration/Mocks.ts
+++ tests/integration/Mocks.ts
@@ -18,3 +18,26 @@ export const CREATECAROUTPUT = {
   doorsQty: 4,
   seatsQty: 5,
 };
+
+export const ALLCARSOUTPUT = [
+  {
+    id: '634852326b35b59438fbea2f',
+    model: 'Marea',
+    year: 2002,
+    color: 'Black',
+    status: true,
+    buyValue: 15.99,
+    doorsQty: 4,
+    seatsQty: 5,
+  },
+  {
+    id: '634852326b35b59438fbea31',
+    model: 'Tempra',
+    year: 1995,
+    color: 'Black',
+    buyValue: 39,
+    doorsQty: 2,
+    seatsQty: 5,
+    status: false,
+  },
+];
--- tests/integration/Routes/CarRoutes.test.ts
+++ tests/integration/Routes/CarRoutes.test.ts
@@ -3,7 +3,7 @@ import Sinon from 'sinon';
 import { expect } from 'chai';
 import { Model } from 'mongoose';
 import app from '../../../src/app';
-import { CARINPUT, CREATECAROUTPUT } from '../Mocks';
+import { ALLCARSOUTPUT, CARINPUT, CREATECAROUTPUT } from '../Mocks';
 
 describe('testa as rotas de Car', function () {
   it('testa se é possivel criar um carro com sucesso', async function () {
@@ -16,8 +16,24 @@ describe('testa as rotas de Car', function () {
 
     Sinon.restore();
   });
+  it('testa quando um erro de servidor é lançado na rota create/cars', async function () {
+    Sinon.stub(Model, 'create').throws();
 
-  // afterEach(function () {
-  //   sinon.restore();
-  // });
+    const response = await request(app).post('/cars').send();
+    expect(response.status).to.be.equal(500);
+    expect(response.body).to.be.deep.equal({ message: 'Error' });
+  });
+
+  it('testa se é possível buscar todos os carros da aplicação', async function () {
+    Sinon.stub(Model, 'find').resolves(ALLCARSOUTPUT);
+
+    const response = await request(app).get('/cars');
+
+    expect(response.status).to.be.equal(200);
+    expect(response.body).to.be.deep.equal(ALLCARSOUTPUT);
+  });
+
+  afterEach(function () {
+    Sinon.restore();
+  });
 });
